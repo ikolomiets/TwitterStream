5aee9837fa1f1875a952e4d2f54adf7baa2937f4
Igor Kolomiets <samolet@gmail.com>
2014-01-25 00:32:38 -0500

    * working version * configure maven-assembly-plugin

--- src/main/java/Streamer.java
+++ src/main/java/Streamer.java
@@ -1,20 +1,48 @@
-import twitter4j.Status;
-import twitter4j.StatusAdapter;
-import twitter4j.TwitterStreamFactory;
+import twitter4j.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Streamer {
 
-    public static void main(String[] args) {
+    private static DateFormat dateFormat = new SimpleDateFormat("HH:mm");
 
-        twitter4j.TwitterStream twitter = new TwitterStreamFactory().getInstance();
-        twitter.addListener(new StatusAdapter() {
+    public static void main(String[] args) throws TwitterException {
+
+        Twitter twitter = TwitterFactory.getSingleton();
+
+
+        List<Long> userIdList = new ArrayList<Long>();
+        for (String arg : args) {
+            if (!arg.startsWith("@"))
+                continue;
+
+            String screenName = arg.substring(1);
+            User user = twitter.users().showUser(screenName);
+            System.out.println("Resolve " + arg + " to " + user.getId());
+            userIdList.add(user.getId());
+        }
+
+        long[] userIds = new long[userIdList.size()];
+        for (int i = 0; i < userIdList.size(); i++)
+            userIds[i] = userIdList.get(i);
+
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(new StatusAdapter() {
             public void onStatus(Status status) {
-                System.out.println(status.getUser().getName() + " : " + status.getText());
+                String createdAt = dateFormat.format(status.getCreatedAt());
+                System.out.println(createdAt + " " + status.getUser().getScreenName() + ": " + status.getText());
             }
         });
-        twitter.sample();
 
+        FilterQuery filterQuery = new FilterQuery();
+        filterQuery.track(args);
+        filterQuery.follow(userIds);
 
+        twitterStream.filter(filterQuery);
     }
 
 }
