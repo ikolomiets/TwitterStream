748d169ba07e150369b68782d0a82d6098f62273
ikolomie <samolet@gmail.com>
2014-05-23 15:25:41 -0400

    * implement TopTwitRatingFetcher.java

---
+++ src/main/java/TopTwitRatingFetcher.java
@@ -0,0 +1,40 @@
+/*
+ * IBM Confidential
+ * OCO Source Materials
+ * 5725H42
+ * Copyright IBM Corp. 2002, 2014
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets, irrespective of what has been
+ * deposited with the U.S. Copyright Office.
+ */
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TopTwitRatingFetcher {
+
+    public static final String baseUrl = "http://toptwit.ru/users/?page=";
+    private static CloseableHttpClient httpclient = HttpClients.createDefault();
+    private static final Pattern regex = Pattern.compile("<a href=\"/users/(\\w+)\" class=\"rating\">");
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        for (int i = 201; i <= 300; i++) {
+            HttpGet httpGet = new HttpGet(baseUrl + i);
+            CloseableHttpResponse response = httpclient.execute(httpGet);
+            String content = EntityUtils.toString(response.getEntity());
+            Matcher matcher = regex.matcher(content);
+            while (matcher.find())
+                System.out.println(matcher.group(1));
+
+            Thread.sleep(1000);
+        }
+    }
+
+}
